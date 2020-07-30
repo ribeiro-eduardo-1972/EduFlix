d011dc14a353c4547e822d4b7d08150897af6137
ribeiro-eduardo-1972 <68924432+ribeiro-eduardo-1972@users.noreply.github.com>
2020-07-30 11:49:03 -0300

    Projeto Dia 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
-import ButtonLink from './components/ButtonLink'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-        <a href="/">
-            <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
-        </a>
-        <ButtonLink className="ButtonLink" href="/">
-            Novo vídeo
-        </ButtonLink>
-        </nav>
-        )    
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
-export default Menu
+export default Menu;
